8e1a2d510fb0f4b099ba4f47209adfd01488f2dd
Rasmus Munk <munk1@live.dk>
2023-05-26 17:06:16 +0200

    Add the rhel dependency install script

---
+++ dep/rhel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# The cloud-localds is used to generate the configuration image
+# for cloud-init
+dnf install -y cloud-utils --enablerepo devel
+
+# genisoimage is required for filesystem=iso9660
+dnf install -y genisoimage
+
+# The emulator used to start and configure the image
+dnf install -y qemu-kvm
+
+# If the link is missing
+ln -s /usr/libexec/qemu-kvm /usr/bin/qemu-kvm
+
+# Used to reset the image before it is deployed
+dnf install -y /usr/bin/virt-sysprep
