bdf96f21eddc5898dc025232a37a42fda108fbd1
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-01-06 07:42:07 +0100

    Add the initial multiple images tests and an associated basic architecture file for testing this

--- tests/test_cli_multiple_image.py
+++ tests/test_cli_multiple_image.py
@@ -15,16 +15,51 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import unittest
+import os
+import random
+from gen_vm_image.common.defaults import MULTIPLE
 from gen_vm_image.common.codes import SUCCESS
+from gen_vm_image.utils.io import exists, makedirs, join, remove
 from gen_vm_image.cli.cli import main
 
+BASIC_ARCHITECTURE_FILE = "basic_architecture.yml"
+BASIC_ARCHITECTURE_PATH = os.path.realpath(join("tests", "res", BASIC_ARCHITECTURE_FILE))
 
 class TestCLIMultipleImage(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.seed = str(random.random())[2:10]
+        cls.images_dir = os.path.realpath(join("tests", "tmp", "images", cls.seed))
+        cls.res_dir = os.path.realpath(join("tests", "res"))
+        if not exists(cls.images_dir):
+            assert makedirs(cls.images_dir)
+
+    @classmethod
+    def tearDownClass(cls):
+        if exists(cls.images_dir):
+            assert remove(cls.images_dir, recursive=True)
 
     def test_cli_help(self):
         return_code = None
         try:
-            return_code = main(["-h"])
+            return_code = main([MULTIPLE, "--help"])
+        except SystemExit as e:
+            return_code = e.code
+        self.assertEqual(return_code, SUCCESS)
+
+    def test_cli_basic_multiple_image(self):
+        return_code = None
+        try:
+            self.assertTrue(exists(BASIC_ARCHITECTURE_PATH))
+            return_code = main(
+                [
+                    MULTIPLE,
+                    BASIC_ARCHITECTURE_PATH,
+                    "--images-output-directory",
+                    self.images_dir,
+                ]
+            )
         except SystemExit as e:
             return_code = e.code
         self.assertEqual(return_code, SUCCESS)
